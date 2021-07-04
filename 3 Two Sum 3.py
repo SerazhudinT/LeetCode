cd1bec9de1c0d661471da8cd609d84f534723215
“SerazhudinT” <serazhudint@gmail.com>
2021-07-04 20:42:21 +0300

    S

--- 3 Two Sum 3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = {}
-
-        for i in range(len(nums)):
-            if numsDict.get(target - nums[i]) != None and numsDict[target - nums[i]] != i:
-                return numsDict[target - nums[i]], i
-            
-            numsDict[nums[i]] = i
